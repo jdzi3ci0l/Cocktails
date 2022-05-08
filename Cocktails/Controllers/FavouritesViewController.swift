045aa16b636196f2c10ecbf8bf5f94997e0c24ac
jdzi3ci0l <j.dzi3ci0l@gmail.com>
2022-05-08 13:35:46 +0200

    Start designing Favourites tab

--- Cocktails/Controllers/FavouritesViewController.swift
+++ Cocktails/Controllers/FavouritesViewController.swift
@@ -8,10 +8,43 @@
 import UIKit
 
 class FavouritesViewController: UIViewController {
+    
+    var cocktails: [Cocktail] = []
+    
+    private let favouritesTableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "Favourites"
         view.backgroundColor = .systemBackground
+        view.addSubview(favouritesTableView)
+        favouritesTableView.delegate = self
+        favouritesTableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        favouritesTableView.frame = view.bounds
+    }
+}
+
+extension FavouritesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Test"
+        return cell
     }
 }
